e4097999b16dbb56833e9961e9158ec4585ed5aa
augustczar <augustczarramos@gmail.com>
2023-11-06 16:06:52 -0300

    Creation of the domain entity

--- Book/src/main/java/br/com/spassu/book/model/BookModel.java
+++ Book/src/main/java/br/com/spassu/book/model/BookModel.java
@@ -1,9 +1,17 @@
 package br.com.spassu.book.model;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,17 +22,29 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class BookModel {
+@Entity
+@Table(name = "BOOK")
+public class BookModel implements Serializable{
 
+	private static final long serialVersionUID = -1588900577343705351L;
+
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private UUID bookId;
 	
+	@Column(nullable = false, unique = true, length = 40)
 	private String title;
 	
+	@Column(nullable = false, length = 40)
 	private String publishingCompany;
 	
+	@Column(nullable = false)
 	private Integer edition;
 	
+	@Column(nullable = false, length = 4)
 	private String yearPublication;
 	
+	@Column(nullable = false)
 	private BigDecimal price;
 }
