1084aa9a20552bc83c0d48cbf9b691f9ced4b7df
augustczar <augustczarramos@gmail.com>
2023-11-09 16:08:24 -0300

    update of the Book-to-Author relationship

--- Author/src/main/java/br/com/spassu/author/model/AuthorModel.java
+++ Author/src/main/java/br/com/spassu/author/model/AuthorModel.java
@@ -1,22 +1,16 @@
 package br.com.spassu.author.model;
 
 import java.io.Serializable;
-import java.util.List;
-import java.util.Set;
 import java.util.UUID;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -45,7 +39,4 @@ public class AuthorModel implements Serializable {
 	@Column(nullable = false, unique = true, length = 40)
 	private String name;
 
-//	@JsonProperty(access = Access.WRITE_ONLY)
-//	@ManyToMany(fetch = FetchType.LAZY)
-//	private Set<> author;
 }
